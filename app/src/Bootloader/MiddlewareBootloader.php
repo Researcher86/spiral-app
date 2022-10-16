fab5477b1647bbf9c537dd63761e65e2b02203b9
Tanat <researcher2286@gmail.com>
2022-10-16 10:36:44 +0600

    Add enable Whoops exception handler with debug

--- app/src/Bootloader/MiddlewareBootloader.php
+++ app/src/Bootloader/MiddlewareBootloader.php
@@ -7,13 +7,16 @@ namespace App\Bootloader;
 use App\Middleware\ValidationExceptionMiddleware;
 use App\Middleware\WhoopsMiddleware;
 use Spiral\Boot\Bootloader\Bootloader;
+use Spiral\Boot\EnvironmentInterface;
 use Spiral\Bootloader\Http\HttpBootloader;
 
 class MiddlewareBootloader extends Bootloader
 {
-    public function boot(HttpBootloader $http): void
+    public function boot(HttpBootloader $http, EnvironmentInterface $environment): void
     {
-        $http->addMiddleware(WhoopsMiddleware::class);
+        if ($environment->get('DEBUG')) {
+            $http->addMiddleware(WhoopsMiddleware::class);
+        }
         $http->addMiddleware(ValidationExceptionMiddleware::class);
     }
 }
